34e67bebc6ccbd0676f971c19c6e5ebcfe02c2bd
Hridoy <ridoy.java@gmail.com>
2023-08-08 16:47:30 +0600

    color picker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { ChromePicker } from "react-color-pick";
 
 function App() {
+  const [color, setColor] = useState("#fff");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChromePicker
+        color={color}
+        onChange={(updateColor) => setColor(updateColor)}
+      />
+      <h2>You picked {color}</h2>
     </div>
   );
 }

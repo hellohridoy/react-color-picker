89960fd4bd663e3890be6fa4126c53bb5d3c62ae
Hridoy <ridoy.java@gmail.com>
2023-08-09 12:18:30 +0600

    react-color-picker

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import { ChromePicker } from "react-color-pick";
 
 function App() {
   const [color, setColor] = useState("#fff");
+  const [showColorPicker, setShowColorPicker] = useState(false);
   return (
-    <div className="App">
-      <ChromePicker
-        color={color}
-        onChange={(updateColor) => setColor(updateColor)}
-      />
+    <div>
+      <button
+        onClick={() =>
+          setShowColorPicker((showColorPicker) => !showColorPicker)
+        }
+      >
+        {showColorPicker ? "close color picker" : "pick a color"}
+      </button>
+      {showColorPicker && (
+        <ChromePicker
+          color={color}
+          onChange={(updateColor) => setColor(updateColor.hex)}
+        />
+      )}
+
       <h2>You picked {color}</h2>
     </div>
   );
